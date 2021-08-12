7b12ac23e4ea71652a72ccb3bfc0766b058550eb
Jeonghwan Yoon <yoonjh517@gmail.com>
2021-08-12 15:40:47 +1000

    encrypt user password by using bcrypt library

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+// bcrypt setting
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,27 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// before save user informtaion
+// don't use arrow function here, follow the instruction...
+userSchema.pre("save", function (next) {
+  var user = this;
+  // only if user changes password
+  if (user.isModified("password")) {
+    // encrypt user password by using bcrypt
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      // hash: encrypted password
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
