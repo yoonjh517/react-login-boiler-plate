32cf1dc53a3e65d55eca7ac8c378c01c8588b155
Jeonghwan Yoon <yoonjh517@gmail.com>
2021-08-12 14:46:36 +1000

    connect to mongodb  and make user schema, model with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://yoonjh517:Zpfhfh12@cluster0.warjd.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
