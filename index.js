8177795be9fd65fbc142a98e6a72110727b330fa
Jeonghwan Yoon <yoonjh517@gmail.com>
2021-08-12 16:22:55 +1000

    generating token for login by jwt library

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = 5000;
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
+app.use(cookieParser());
 
 mongoose
   .connect(config.mongoURI, {
@@ -32,6 +34,32 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // find the requsted email in database
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "There's no user who has the requested id",
+      });
+    }
+    // if the requested email data is in the database, check if the password is right
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+      // if the password is right, generate a token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // save the token  into cookie by using cookie-parser
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 // bcrypt setting
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+// jwt
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -54,6 +56,28 @@ userSchema.pre("save", function (next) {
     next();
   }
 });
+
+// comparing password function for login request
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // encrypt plain password to compare with the password data from database, cb: callback
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// generating token function for login request
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  // generate token by using jsonwebtoken
+  // user._id + 'secretToken' = token
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
