e420bedd6c9ec23a18b76c6d6800b2065b0091a2
Jeonghwan Yoon <yoonjh517@gmail.com>
2021-08-13 14:00:18 +1000

    login page form setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,7 +6,19 @@ function LandingPage() {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
-  return <div>Landing Page</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>Landing Page</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function LoginPage() {
-  return <div>Login Page</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault(); // to prevent reload page
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
