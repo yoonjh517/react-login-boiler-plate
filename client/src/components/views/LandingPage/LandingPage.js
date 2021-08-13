f2a8b8bcd11ca6f1bbe1e2ddf284f595efe8b731
Jeonghwan Yoon <yoonjh517@gmail.com>
2021-08-13 15:32:21 +1000

    logout setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to log out");
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +27,7 @@ function LandingPage() {
       }}
     >
       <h2>Landing Page</h2>
+      <button onClick={onClickHandler}>Logout</button>
     </div>
   );
 }
